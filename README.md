7351738b8f81ec2936fe828399e92c944643a50e
Dan Stormont <daniel.stormont@gmail.com>
2014-09-21 15:58:03 -0700

    1 - Combine test and training files
    Initial commit of a script that combines the test and training files
    into a single data frame. (Step 1 of the assignment instructions.)

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ TidyDataProject
 ===============
 
 This repo contains the source code, data, and documentation for the Coursera Getting and Cleaning Data Course Project.
+
+The script assumes that the zipped file for the assignment has been unzipped in the same directory as the run_analysis.R file.
+
+The script reads the two "X" files in the UCI HAR Dataset folder and combines them into a single dplyr data frame.
---
+++ run_analysis.R
@@ -0,0 +1,25 @@
+## run_analysis.R
+## 
+## This script will load the necessary libraries and run the analysis needed to
+## produce a tidy data set from the Human Activity Recognition Using Smartphones
+## dataset described at http://archive.ics.uci.edu/ml/dataset/Human+Activity+
+## Recognition+Using+Smartphones.
+##
+## The script assumes that the raw data set is in the working directory.
+
+## Install the libraries that will be used to tidy the data set.
+library(plyr)
+library(dplyr)
+library(tidyr)
+
+## This function will read the smartphone data set and generate a tidy data set.
+run_analysis <- function() {
+    ## Start by reading in the test and training data set of the phone 
+    ## measurements and merging them into a single table.
+    phoneData <- merge(read.table("./UCI HAR Dataset/test/X_test.txt"),
+                       read.table("./UCI HAR Dataset/train/X_train.txt"),
+                       all = TRUE)
+    ## Make the resulting table into a plyr data frame for ease of use.
+    phoneData <- tbl_df(phoneData)
+    phoneData
+}
